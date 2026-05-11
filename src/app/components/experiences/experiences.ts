7a85c780bf12e2fdb4227d0655853941f27f84c2
Programmer-Emmanuel <marcbamidele@gmail.com>
2026-05-11 19:14:33 +0000

    Mise à jour CV et competences

--- src/app/components/experiences/experiences.ts
+++ src/app/components/experiences/experiences.ts
@@ -13,19 +13,24 @@ export class Experiences implements OnInit {
 
   experiences = [
     {
+      titre: 'Stage à ProActive Swiss',
+      description: '2eme place à l’académie des jeunes developpeurs Fullstack',
+      annee: 'Mai 2026 à Aujourd’hui'
+    },
+    {
       titre: 'BootCamp à ProActive Swiss et Growing Consulting',
       description: '2eme place à l’académie des jeunes developpeurs Fullstack',
-      annee: '2026'
+      annee: 'Avril 2026'
     },
     {
       titre: 'Stage à NEURA COMPUTING',
       description: 'Développeur fullstack',
-      annee: '2025'
+      annee: 'Juin à Août 2025'
     },    
     {
       titre: 'CONCOURS ET CHALLENGE TECH',
       description: '2ème Place au YADAC ROBOTICS CHALLENGE',
-      annee: '2024'
+      annee: 'Décembre 2024'
     },
   ];
 
